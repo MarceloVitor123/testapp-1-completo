af960974f31fb3e6cb8bc732fe9de2b8c95aded8
LAP235-jpg <lapa86002@gmail.com>
2026-09-22 10:03:41 -0300

    atividade 2 pronta

---
+++ app/fases/fase2/atividade3.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import MarkActivity from "../../../components/templates/MarkActivity";
+
+export default function Atividade2Screen() {
+  return (
+   <MarkActivity
+  question='QUAIS SILABAS FORMA A "PALAVRA" PANELA'
+  options={[
+    { label: "PA", value: "u" },
+    { label: "NE", value: "e" },
+    { label: "GA", value: "a" },
+    { label: "PE", value: "b" },
+    { label: "LA", value: "l" },
+    { label: "MAR", value: "k" },
+  ]}
+  correctAnswers={["e","u","l"]}
+  nextRoute="/fases/fase2/atividade4"
+  wrongRoute="/fases/fase2/atividade4"
+  progress={0.4}
+/>
+  );
+}
---
+++ app/fases/fase2/atividade4.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import MarkActivity from "../../../components/templates/MarkActivity";
+
+export default function Atividade2Screen() {
+  return (
+   <MarkActivity
+  question='QUAIS SILABAS FORMA A PALAVRA "CACHORRO"?'
+  options={[
+    { label: "RO", value: "u" },
+    { label: "RRO", value: "e" },
+    { label: "CHOR", value: "a" },
+    { label: "FOI", value: "b" },
+    { label: "AU", value: "l" },
+    { label: "CA", value: "k" },
+  ]}
+  correctAnswers={["a","u","k"]}
+  nextRoute="/fases/fase2/atividade5"
+  wrongRoute="/fases/fase2/atividade5"
+  progress={0.6}
+/>
+  );
+}
